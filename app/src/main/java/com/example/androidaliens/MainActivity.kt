7946f0c76a96928d7c1fbea4271c825d58996c10
Mohammed <mohammedkhudair57@gmail.com>
2024-03-25 12:10:35 +0300

    My forth commit 2

--- app/src/main/java/com/example/androidaliens/MainActivity.kt
+++ app/src/main/java/com/example/androidaliens/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     AndroidAliens()
                     // new commit
 
-                    this is error
+                    this is commit
                 }
             }
         }
