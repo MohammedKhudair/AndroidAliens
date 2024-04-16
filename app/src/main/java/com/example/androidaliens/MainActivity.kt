de498031e18e037d65b73f158cf6aa70fe2f8d86
Mohammed <mohammedkhudair57@gmail.com>
2024-04-16 09:03:16 +0300

    change strign commit

--- app/src/main/java/com/example/androidaliens/MainActivity.kt
+++ app/src/main/java/com/example/androidaliens/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun BrnchTest(){
-        println("Print BranchTest")
+        println("Print BranchTest from BranchTest")
     }
 }
 
