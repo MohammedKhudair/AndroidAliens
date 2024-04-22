74d6a3b927bff662c255d787a398d5812c2ff9cd
Mohammed <mohammedkhudair57@gmail.com>
2024-04-22 09:52:32 +0300

    Add fun to display the quoute

--- app/src/main/java/com/example/androidaliens/Qoute.kt
+++ app/src/main/java/com/example/androidaliens/Qoute.kt
@@ -1,2 +1,12 @@
 package com.example.androidaliens
 
+val qoutes = listOf(
+    "Practice makes perfect",
+    "No pain no gain",
+    "Believe you can and you'll half way there")
+
+fun DisplayQoute(){
+     qoutes.forEach{
+         println(it)
+     }
+}
