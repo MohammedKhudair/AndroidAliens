b8823e4b7cd2f82c55930d63beb167e409db98d2
‪Mohammed khudair‬‏ <40716035+MohammedKhudair@users.noreply.github.com>
2024-04-23 08:27:36 +0300

    Update Qoute.kt
    edit some quotes

--- app/src/main/java/com/example/androidaliens/Qoute.kt
+++ app/src/main/java/com/example/androidaliens/Qoute.kt
@@ -3,8 +3,8 @@ package com.example.androidaliens
 val qoutes = listOf(
     "Practice makes perfect",
     "No pain no gain",
-    "Believe you can and you'll half way there",
-    "Exposir to hits is the proprate way to learn")
+    "Believe you can and you'll be halfway there",
+    "Exposure to hits is the appropriate way to learn")
 
 fun DisplayQoute(){
      qoutes.forEach{
